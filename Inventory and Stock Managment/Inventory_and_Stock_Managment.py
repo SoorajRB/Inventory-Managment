96d351d6de541f61644f38d7ee46a5e18baa618c
sooraj <sooraj.rahul@gmail.com>
2018-07-06 11:12:34 +0530

    Inventory Complete

--- Inventory and Stock Managment/Inventory_and_Stock_Managment.py
+++ Inventory and Stock Managment/Inventory_and_Stock_Managment.py
@@ -34,6 +34,8 @@ def inventory():
         additem()
     if(choice)==2:
         deleteitem()
+    if(choice)==3:
+        viewitem()
 
 def deleteitem():
     os.system('cls')
@@ -73,44 +75,66 @@ def deleteitem():
         print e
         db.rollback()
         
-
-    
-    
-
-
 def additem():
     
     while(1):
-        os.system('cls')
-        slno = int(input("Enter the Serial Number: "))
-        itemname = raw_input("Enter the itemname: ")
-        price = float(input("Enter the price: "))
-        quantity = int(input("Enter the quantity: "))
-        expdate = raw_input("Enter the date YYYY-MM-DD: ")
-        GST = float(input("Enter the GST Rate: "))
-
-        total = (price * GST/100) + price
-
-        sql = 'Insert into itemdetails (slno,itemname,price,quantity,ExpiryDate,GST,total) values (%d,"%s",%f,%d,"%s",%f,%f)' % (slno,itemname,price,quantity,expdate,GST,total) 
-
         try:
-            cursor.execute(sql)
-            db.commit()
-            print("Item is inserted into db")
-        except MySQLdb.Error,e:
-            print e
-            db.rollback()
+            os.system('cls')
+            slno = int(input("Enter the Serial Number: "))
+            itemname = raw_input("Enter the itemname: ")
+            price = float(input("Enter the price: "))
+            quantity = int(input("Enter the quantity: "))
+            expdate = raw_input("Enter the date YYYY-MM-DD: ")
+            GST = float(input("Enter the GST Rate: "))
+
+            total = (price * GST/100) + price
+
+            sql = 'Insert into itemdetails (slno,itemname,price,quantity,ExpiryDate,GST,total) values (%d,"%s",%f,%d,"%s",%f,%f)' % (slno,itemname,price,quantity,expdate,GST,total) 
+
+            try:
+                cursor.execute(sql)
+                db.commit()
+                print("Item is inserted into db")
+            except MySQLdb.Error,e:
+                print e
+                db.rollback()
         
-        choice = raw_input("Add next time?Y/N")
-        if choice=='y' or choice=='Y':
-            additem()
-        else: 
-            for i in range (0,3):
-                print("Returning to inventory in...", i)
-                time.sleep(3)
-                inventory()
-    
-    
+            choice = raw_input("Add next item?Y/N")
+            if choice=='y' or choice=='Y':
+                additem()
+            else: 
+                for i in range (0,3):
+                    print("Returning to inventory in...", i)
+                    time.sleep(1)
+                    inventory()
+        except Exception, e:
+             print e
+             Inventory()
+   
+def viewitem():
+        sql="select * from itemdetails"
+        print sql
+        cursor.execute(sql)
+        db.commit()
+        data = cursor.fetchall()
+        print("---------------------------------------------------------------")
+        print("Sl NO |  ItemName  |  Price  |  Quantity  |\tExpdate\t|GST|Total")
+        print("---------------------------------------------------------------")
+        for row in data:
+            slno = row[0]
+            itemname = row[1]
+            price = row[2]
+            quantity = row[3]
+            expdate = str(row[4])
+            gst = row[5]
+            total = row[6]
+          
+            print slno,"      " , itemname,"        ", price,"     ", quantity,"     ", expdate,"   ", gst,"   ", total 
+           
+        choice = raw_input("Go back to inventory?Y/N")
+        if choice == 'Y' or choice == 'y':
+            inventory()
+
 
 if __name__ == '__main__':
         main()
